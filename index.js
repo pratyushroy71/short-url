ab0c6ef4afd6427896f103fe5af467804f6a5beb
Pratyush Roy <pratyush.roy.71@gmail.com>
2026-01-13 22:26:33 +0530

    ejs implementation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const staticRouter = require("./routes/staticRouter");
 const urlRoute = require('./routes/url');
 const { connectToMongoDB } = require("./connect");
 const URL = require('./models/url');
@@ -7,10 +9,27 @@ const PORT = 8001;
 
 connectToMongoDB('mongodb://localhost:27017/short-url');
 
+app.set("view engine" , "ejs");
+app.set('views' , path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/test' , async(req, res) => {
+    const allUrls = await URL.find({});
+    return res.render('home',{
+        urls: allUrls,
+         host: req.headers.host
+    });
+});
 
 
 app.use("/url", urlRoute);
+
+//app.use("/", staticRouter);
+
+
+
 
 app.get('/:shortId', async (req, res) => {
     try {
